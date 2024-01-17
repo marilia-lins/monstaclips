e15358ca88758b4b4e5e0da4c734e0c0ee7a46b0
Marilia Lins <marilia.siqueira.lins@gmail.com>
2024-01-17 14:45:19 -0300

    remoção do footer

--- src/Pages/BasicPage/index.jsx
+++ src/Pages/BasicPage/index.jsx
@@ -11,7 +11,7 @@ const BasicPage = () => {
                 <FavoritesProvider>
                     <Outlet/>
                 </FavoritesProvider>
-            <Footer/>
+            
         </main>
 
     )
