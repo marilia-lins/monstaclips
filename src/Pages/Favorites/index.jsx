c1dd6ff10690441c69d2f0693e5b8c43d3c5129c
Marilia Lins <marilia.siqueira.lins@gmail.com>
2024-01-17 15:30:14 -0300

    nova atualização

--- src/Pages/Favorites/index.jsx
+++ src/Pages/Favorites/index.jsx
@@ -10,29 +10,15 @@ const Favorites = () => {
 
     return(
         <>
-            { favorite === null ? (
-                <>
-                    <Banner image='header' />
-                    <Title>
-                        Clipes Favoritados!
-                    </Title>
-                    <section className={styles.fave}>
-                        {favorite.map((fav) => {
-                            return <Card {...fav} key={fav.id} />
-                        })}
-                    </section>
-                </>
-            ) : (
-                <>
-                    <Banner image='header' />
-                    <Title>
-                        Clipes Favoritados!
-                    </Title>
-                    <section className={styles.fave}>
-                        <h4>Nenhum clipe favoritado ainda</h4>
-                    </section>
-                </>
-            )}
+            <Banner image='header' />
+            <Title>
+                Clipes Favoritados!
+            </Title>
+            <section className={styles.fave}>
+                {favorite.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
+            </section>
         </>
     )
 }
